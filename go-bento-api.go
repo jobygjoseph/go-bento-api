c20bdf51c461c17a665edd825565acfbd28c901d
Joby Joseph <joby.joseph@nbcuni.com>
2018-12-31 23:42:05 -0500

    Working but without context - need to pass store via context

--- go-bento-api.go
+++ go-bento-api.go
@@ -1,20 +1,25 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/go-bento-api/schemas"
+	"github.com/go-bento-api/store"
 	"github.com/graphql-go/graphql"
 	"github.com/graphql-go/handler"
 )
 
 func main() {
 
-	//APIstore := store.CreateStore()
+	APIstore := store.CreateStore()
 	//fmt.Println(APIstore.Video.FindByID("mmvo16656963832"))
 	// fmt.Println(APIstore.Video.FindByID("mmvo16656963832"))
 
-	description := "![NBC News](http://sslnodeassets.nbcnews.com/cdnassets/projects/site-images/nbcnews-logo-white.png \"NBC NEWS\")The Graphiql IDE for the Bento API"
+	description := "![NBC News](http://sslnodeassets.nbcnews.com/cdnassets/projects/site-images/nbcnews-logo-white.png \"NBC NEWS\")\n\nThe Graphiql IDE for Bento API"
 
 	// Schema
 	fields := graphql.Fields{"Video": schemas.VideoSchema}
@@ -31,6 +36,32 @@ func main() {
 		GraphiQL: true,
 	})
 
+	handleGraphqlRequest := func(w http.ResponseWriter, r *http.Request) {
+		ctx := context.WithValue(context.Background(), "APIstore", APIstore)
+		if err := r.ParseForm(); err != nil {
+			fmt.Fprintf(w, "ParseForm() err: %v", err)
+			return
+		}
+		Query := r.FormValue("query")
+		//Variables := r.FormValue("variables")
+		OperationName := r.FormValue("operationName")
+		params := graphql.Params{
+			RequestString: Query,
+			//VariableValues: Variables,
+			OperationName: OperationName,
+			Context:       ctx,
+			Schema:        schema}
+		result := graphql.Do(params)
+		if len(result.Errors) > 0 {
+			log.Fatalf("failed to execute graphql operation, errors: %+v", result.Errors)
+		}
+		rJSON, _ := json.Marshal(result)
+		fmt.Fprintf(w, string(rJSON))
+		//w.Write(string(rJSON))
+	}
+
+	//http.HandleFunc()
 	http.Handle("/graphql", h)
+	http.HandleFunc("/", handleGraphqlRequest)
 	http.ListenAndServe(":8080", nil)
 }
